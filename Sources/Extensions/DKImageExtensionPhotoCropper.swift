6927e13ef288b9599abf6eac82ef14577b6bf53b
zhangao0086 <zhangao0086@gmail.com>
2024-04-17 11:56:34 +0800

    Fixed CropViewController import.

--- Sources/Extensions/DKImageExtensionPhotoCropper.swift
+++ Sources/Extensions/DKImageExtensionPhotoCropper.swift
@@ -32,7 +32,12 @@ open class DKImageExtensionPhotoCropper: DKImageBaseExtension {
         self.metadata = extraInfo["metadata"] as? [AnyHashable : Any]
         self.didFinishEditing = didFinishEditing
         
+#if canImport(CropViewController)
         let imageCropper = CropViewController(image: sourceImage)
+#else
+        let imageCropper = TOCropViewController(image: sourceImage)
+#endif
+        
         imageCropper.onDidCropToRect = { [weak self] image, _, _ in
             guard let strongSelf = self else { return }
             
